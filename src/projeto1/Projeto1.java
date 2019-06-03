25c36baaa17f57b2f4c5004696d9055b48dae130
Aluno <aluno@aluno>
2019-06-03 11:11:35 -0300

    Adicao ao Git

---
+++ src/projeto1/Projeto1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Projeto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Janela1 j1 = new Janela1();
+        j1.setVisible(true);
+    }
+    
+}
